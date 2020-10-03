c13c6c53f23d6de93b4b124ae43059c556d6ca26
Marcello Seri <marcello.seri@gmail.com>
2020-10-03 21:07:52 +0200

    Format code
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/backend.mli
+++ src/backend.mli
@@ -5,9 +5,9 @@
 (* http://www.isc.org/downloads/software-support-policy/isc-license/   *)
 (***********************************************************************)
 
-val mld_of_md : min_header:int -> Omd_representation.t -> string
 (** [mld_of_md ~min_header md] returns a string containing the mld version of
     [md]. Note that [md] uses the {!Omd} internal representation of
     Markdown. The ~min_header parameters allows you to choose the value
     for the section heading associated to <h1>, all the other values will
     be determined incrementally. *)
+val mld_of_md : min_header:int -> Omd_representation.t -> string
--- src/md2mld.ml
+++ src/md2mld.ml
@@ -10,8 +10,14 @@ let readall filename =
   try
     let n = in_channel_length ic in
     let b = Bytes.create n in
-    really_input ic b 0 n ; close_in ic ; Bytes.to_string b
-  with exn -> close_in ic ; raise exn
+    really_input ic b 0 n;
+    close_in ic;
+    Bytes.to_string b
+  with
+  | exn ->
+    close_in ic;
+    raise exn
+
 
 (*
 let writeall filename content =
@@ -28,26 +34,27 @@ let _ =
   let speclist =
     [ ( "-min-header"
       , Arg.Set_int min_header
-      , "Minimal section header level. Defaults to 0." ) ]
+      , "Minimal section header level. Defaults to 0." )
+    ]
   in
   let usage_msg =
     "Usage: md2mld [OPTIONS] FILENAME\n\
-     md2mld converts a Markdown-format file into the mld format used by odoc \
-     to render HTML documentation or OCaml libraries.\n\n\
+     md2mld converts a Markdown-format file into the mld format used by odoc to render \
+     HTML documentation or OCaml libraries.\n\n\
      Options:"
   in
-  Arg.parse speclist
-    (fun filename -> input_files := filename :: !input_files)
-    usage_msg ;
+  Arg.parse speclist (fun filename -> input_files := filename :: !input_files) usage_msg;
   match !input_files with
   | [] | _ :: _ :: _ ->
-      Arg.usage speclist usage_msg ;
-      exit 1
-  | [filename] when not (Sys.file_exists filename) ->
-      Printf.eprintf "error: file %s not found\n%!" filename ;
-      exit 2
-  | [filename] ->
-      let min_header = !min_header in
-      readall filename |> Omd.of_string
-      |> Backend.mld_of_md ~min_header
-      |> String.trim |> print_endline
+    Arg.usage speclist usage_msg;
+    exit 1
+  | [ filename ] when not (Sys.file_exists filename) ->
+    Printf.eprintf "error: file %s not found\n%!" filename;
+    exit 2
+  | [ filename ] ->
+    let min_header = !min_header in
+    readall filename
+    |> Omd.of_string
+    |> Backend.mld_of_md ~min_header
+    |> String.trim
+    |> print_endline
